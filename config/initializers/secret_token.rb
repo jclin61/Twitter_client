4f2bff85c5f4a89d5b7ebaacaf10d8a9097ce77f
Jerry C. Lin <jerryclin@hotmail.com>
2014-01-20 13:27:00 -0800

    first commit, gemfile updated 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterJclin::Application.config.secret_key_base = 'ebb4eebad8f7dfa7474ecaa69cec73abdb4e8df70cbe485b896668921a1d6f6c4a9a5d2b796be1d982c0c13831e4b55b5dee743e9acbe03f396ccb0dd3d6092e'
