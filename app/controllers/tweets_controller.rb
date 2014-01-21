cb49d82a01985c27742490350649fc65b416c57a
Jerry C. Lin <jerryclin@hotmail.com>
2014-01-21 13:00:11 -0800

    REST API home_timeline working

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,9 @@
 class TweetsController < ApplicationController
   def index
+  	#specify client.user's info as @user
   	@user = client.user(include_entities: true)
-  end
+
+  	#user's home_timeline passed through @tweets for index page
+  	@tweets = client.home_timeline
+  end  
 end
