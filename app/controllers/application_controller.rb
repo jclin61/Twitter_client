d206a21680628921aac0423bb5b6abf71c6f2163
Jerry C. Lin <jerryclin@hotmail.com>
2014-01-22 09:44:27 -0800

    altered consumer key/secret config to accept preset authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,25 @@ class ApplicationController < ActionController::Base
 
 private
 
+  # def client
+  #   @client ||= Twitter::REST::Client.new do |config|
+  #     config.consumer_key = ENV['CONSUMER_KEY']
+  #     config.consumer_secret = ENV['CONSUMER_SECRET']
+  #     config.access_token = session['access_token']
+  #     config.access_token_secret = session['access_token_secret']
+  #   end
+  # end
+
+  # BELOW CONFIG FOR HEROKU
+
   def client
     @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['CONSUMER_KEY']
-      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.consumer_key = "Tkcz4gex3W9FP70jSI5tzg"
+      config.consumer_secret = "EZoeYxCrUVPl4RrRBMoVOQqq2fEmgMoV3sTnEss9g"
       config.access_token = session['access_token']
       config.access_token_secret = session['access_token_secret']
     end
   end
-
-
 
   # CONSUMER KEY reference for checking.
   #     config.consumer_key        = "Tkcz4gex3W9FP70jSI5tzg"
