6f132f8e6ea3f7bd7fad8fba6ee6f0e25ff2a38a
Jerry C. Lin <jerryclin@hotmail.com>
2014-01-22 09:38:37 -0800

    working version.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,13 +18,9 @@ private
 
 
   # CONSUMER KEY reference for checking.
-  # def client
-  #   client = Twitter::REST::Client.new do |config|
   #     config.consumer_key        = "Tkcz4gex3W9FP70jSI5tzg"
   #     config.consumer_secret     = "EZoeYxCrUVPl4RrRBMoVOQqq2fEmgMoV3sTnEss9g"
   #     config.access_token        = "1692654691-kQq5EpTPRB8icJjUuPjppmWq7rd0kYT3EXu1tFA"
   #     config.access_token_secret = "34MXVUrM7lqn5Gracb9dCz1oMI3tWdrHojO2HWNZ4gxge"
-  #   end
-  # end
 
 end
